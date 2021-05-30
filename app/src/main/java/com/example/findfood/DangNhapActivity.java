84d52ce8d17f0f405f403163b4f2163d0bd8fe32
Thien-dtu <thycaouyenhuynh331@gmail.com>
2021-05-30 23:50:54 +0700

    fix activity DangNhap

--- app/src/main/java/com/example/findfood/DangNhapActivity.java
+++ app/src/main/java/com/example/findfood/DangNhapActivity.java
@@ -242,7 +242,7 @@ public class DangNhapActivity extends AppCompatActivity implements GoogleApiClie
                 if (task.isSuccessful()) {
                     for (int i = 0; i < datastore.size(); i++) {
                         if (datastore.get(i).getEmail().equalsIgnoreCase(email.toString()) && datastore.get(i).getPassword().equalsIgnoreCase(password.toString())) {
-                            Toast.makeText(getApplicationContext(), "Login Thành Công", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Đăng Ký Thành Công", Toast.LENGTH_SHORT).show();
                             Intent is = new Intent(getApplicationContext(),MainActivity.class);
                             startActivity(is);
                             break;
